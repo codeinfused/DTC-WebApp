14e1cbaeaa47e60fc7998b07d4b6e99c98d887e5
Mike Smotherman <mike@codeinfused.com>
2017-07-04 09:18:18 -0400

    Update utf8 setting

--- apiapp/source_api/dependencies.php
+++ apiapp/source_api/dependencies.php
@@ -42,7 +42,7 @@ $container['logger'] = function($c)
 $container['db'] = function($c)
 {
   $settings = $c->get('settings')['db'];
-  $pdo = new PDO("mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'], $settings['user'], $settings['pass']);
+  $pdo = new PDO("mysql:host=" . $settings['host'] . ";charset=utf8;dbname=" . $settings['dbname'], $settings['user'], $settings['pass']);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
