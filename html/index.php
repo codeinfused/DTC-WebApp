e9250b324a79177891e4c29b5bf382d405e1baa1
Mike Smotherman <mike@codeinfused.com>
2017-07-06 18:01:53 -0400

    Cache versioning

--- html/index.php
+++ html/index.php
@@ -66,6 +66,6 @@
   <div id="toasts-wrapper"></div>
 
   <script defer src="/vendors/axios.js"></script>
-  <script defer src='<?php if(APP_ENV==='LOCAL'){ echo "/app/app.js"; }else{ echo "/app/app.min.js"; } ?>'></script>
+  <script defer src='<?php if(APP_ENV==='LOCAL'){ echo "/app/app.js"; }else{ echo "/app/app.min.js?v=1"; } ?>'></script>
 </body>
 </html>
